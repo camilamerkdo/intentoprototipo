b9a3621923232afd985bc0bf6a59d2e796437872
camilamerkdo <103153623+camilamerkdo@users.noreply.github.com>
2024-06-04 16:00:54 -0300

    sketch.js

--- sketch.js
+++ sketch.js
@@ -93,7 +93,7 @@ function draw() {
 }
 */
 
-
+/*
 //avances hasta el 2.06.2024
 
 //capas
@@ -194,5 +194,213 @@ function draw() {
   image(Capalineas, 0, 0);
   image(CapamanchaG, 0, 0);
 }
+*/
 
+//avances hasta el 4.06.2024
+// Capas
+let CapamanchaG;
+let CapamanchaN;
+let Capalineas;
+
+// Cantidad es asignada con un valor de 5
+let cant = 5;
+
+// Arreglos
+let manchaG = [];
+let manchaN = [];
+let lineas = [];
+let conteoG = [];
+let conteoN = [];
+let conteoLineas = [];
+// Para la rotacion
+let posG = [];
+let anguloG = [];
+let posN = [];
+let anguloN = [];
+let posLineas = [];
+let anguloLineas = [];
+let anguloN_out = [];
+let anguloLineas_out = [];
+let anguloG_out = [];
+
+// Contadores
+let tiempoDentroCapaN = 0;
+let tiempoDentroCapaG = 0;
+let tiempoDentroCapaL = 0;
+let tiempoAnterior = 0;
+let tiempoLimite = 1000; // 1 segundo
+let tiempoRotacion = 2000; // 2 segundos
+let limiteImagenes = 5; // Las veces que se van a dibujar
+
+let rotarG = false;
+let rotarN = false;
+let rotarLineas = false;
+
+function preload() {
+  // Manchas grandes
+  for (let i = 0; i < cant; i++) {
+    let manchagris = "data/manchasg" + (i + 1) + ".png";
+    manchaG[i] = loadImage(manchagris);
+    conteoG[i] = 0;
+  }
+  // Manchas negras
+  for (let i = 0; i < cant; i++) {
+    let manchanegra = "data/manchasn" + (i + 1) + ".png";
+    manchaN[i] = loadImage(manchanegra);
+    conteoN[i] = 0;
+  }
+  // Líneas
+  for (let i = 0; i < cant; i++) {
+    let dlineas = "data/Linea" + (i + 1) + ".png";
+    lineas[i] = loadImage(dlineas);
+    conteoLineas[i] = 0;
+  }
+}
+
+function setup() {
+  createCanvas(550, 800);
+  // Se convierten en capas
+  CapamanchaG = createGraphics(550, 800);
+  CapamanchaN = createGraphics(550, 800);
+  Capalineas = createGraphics(550, 800);
+}
+
+function draw() {
+  background(150);
+
+  let tiempoTranscurrido = millis() - tiempoAnterior;
+  tiempoAnterior = millis();
+
+  if (mouseY > 0 && mouseY < 266) {
+    tiempoDentroCapaN += tiempoTranscurrido;
+    tiempoDentroCapaG = 0;
+    tiempoDentroCapaL = 0;
+    rotarG = false;
+    rotarLineas = false;
+  } else if (mouseY > 532 && mouseY < 800) {
+    tiempoDentroCapaL += tiempoTranscurrido;
+    tiempoDentroCapaG = 0;
+    tiempoDentroCapaN = 0;
+    rotarG = false;
+    rotarN = false;
+  } else if (mouseY > 266 && mouseY < 532) {
+    tiempoDentroCapaG += tiempoTranscurrido;
+    tiempoDentroCapaN = 0;
+    tiempoDentroCapaL = 0;
+    rotarN = false;
+    rotarLineas = false;
+  } else {
+    tiempoDentroCapaN = 0;
+    tiempoDentroCapaG = 0;
+    tiempoDentroCapaL = 0;
+    rotarG = false;
+    rotarN = false;
+    rotarLineas = false;
+  }
+
+  // Verifica si el mouse está sobre CapamanchaN
+  if (mouseY > 0 && mouseY < 266 && conteoN.reduce((a, b) => a + b, 0) < limiteImagenes) {
+    if (tiempoDentroCapaN >= tiempoLimite) {
+      let i = floor(random(cant));
+      let x = random(width);
+      let y = random(height);
+      let w = random(150, 250);
+      let h = random(150, 250);
+      CapamanchaN.image(manchaN[i], x, y, w, h);
+      conteoN[i]++;
+      posN.push({ x, y, w, h });
+      anguloN.push(0);
+      tiempoDentroCapaN = 0;
+    }
+  } else if (mouseY > 532 && mouseY < 800 && conteoLineas.reduce((a, b) => a + b, 0) < limiteImagenes) {
+    if (tiempoDentroCapaL >= tiempoLimite) {
+      let i = floor(random(cant));
+      let x = random(width);
+      let y = random(height);
+      let w = random(5, 50);
+      let h = random(5, 50);
+      Capalineas.image(lineas[i], x, y, w, h);
+      conteoLineas[i]++;
+      posLineas.push({ x, y, w, h });
+      anguloLineas.push(0);
+      tiempoDentroCapaL = 0;
+    }
+  } else if (mouseY > 266 && mouseY < 532 && conteoG.reduce((a, b) => a + b, 0) < limiteImagenes) {
+    if (tiempoDentroCapaG >= tiempoLimite) {
+      let i = floor(random(cant));
+      let x = random(width);
+      let y = random(height);
+      let w = random(250, 450);
+      let h = random(250, 450);
+      CapamanchaG.image(manchaG[i], x, y, w, h);
+      conteoG[i]++;
+      posG.push({ x, y, w, h });
+      anguloG.push(0);
+      tiempoDentroCapaG = 0;
+    }
+  }
+
+  // Verificar si el mouse se quedo en la misma capa por 2 segundos
+  if (mouseY > 0 && mouseY < 266 && conteoN.reduce((a, b) => a + b, 0) >= limiteImagenes && tiempoDentroCapaN >= tiempoRotacion) {
+    rotarN = true;
+  } else if (mouseY > 532 && mouseY < 800 && conteoLineas.reduce((a, b) => a + b, 0) >= limiteImagenes && tiempoDentroCapaL >= tiempoRotacion) {
+    rotarLineas = true;
+  } else if (mouseY > 266 && mouseY < 532 && conteoG.reduce((a, b) => a + b, 0) >= limiteImagenes && tiempoDentroCapaG >= tiempoRotacion) {
+    rotarG = true;
+  }
+
+  // Rotar imágenes si se alcanzo el límite de imágenes y pasaron 2 segundos
+  CapamanchaN.clear();
+  Capalineas.clear();
+  CapamanchaG.clear();
+
+  if (rotarN) {
+    for (let i = 0; i < posN.length; i++) {
+      anguloN[i] += 0.01;
+      CapamanchaN.push();
+      CapamanchaN.translate(posN[i].x + posN[i].w / 2, posN[i].y + posN[i].h / 2);
+      CapamanchaN.rotate(anguloN[i]);
+      CapamanchaN.image(manchaN[i % cant], -posN[i].w / 2, -posN[i].h / 2, posN[i].w, posN[i].h);
+      CapamanchaN.pop();
+    }
+  } else {
+    for (let i = 0; i < posN.length; i++) {
+      CapamanchaN.image(manchaN[i % cant], posN[i].x, posN[i].y, posN[i].w, posN[i].h);
+    }
+  }
+
+  if (rotarLineas) {
+    for (let i = 0; i < posLineas.length; i++) {
+      anguloLineas[i] += 0.01;
+      Capalineas.push();
+      Capalineas.translate(posLineas[i].x + posLineas[i].w / 2, posLineas[i].y + posLineas[i].h / 2);
+      Capalineas.rotate(anguloLineas[i]);
+      Capalineas.image(lineas[i % cant], -posLineas[i].w / 2, -posLineas[i].h / 2, posLineas[i].w, posLineas[i].h);
+      Capalineas.pop();
+    }
+  } else {
+    for (let i = 0; i < posLineas.length; i++) {
+      Capalineas.image(lineas[i % cant], posLineas[i].x, posLineas[i].y, posLineas[i].w, posLineas[i].h);
+    }
+  }
+
+  if (rotarG) {
+    for (let i = 0; i < posG.length; i++) {
+      anguloG[i] += 0.01;
+      CapamanchaG.push();
+      CapamanchaG.translate(posG[i].x + posG[i].w / 2, posG[i].y + posG[i].h / 2);
+      CapamanchaG.rotate(anguloG[i]);
+      CapamanchaG.image(manchaG[i % cant], -posG[i].w / 2, -posG[i].h / 2, posG[i].w, posG[i].h);
+      CapamanchaG.pop();
+    }
+  } else {
+    for (let i = 0; i < posG.length; i++) {
+      CapamanchaG.image(manchaG[i % cant], posG[i].x, posG[i].y, posG[i].w, posG[i].h);
+    }
+  }
+
+  image(CapamanchaN, 0, 0);
+  image(Capalineas, 0, 0);
+  image(CapamanchaG, 0, 0);
+}
 
